6ab1c1e0784552d10e3210fb259e80153fdee182
shahgithubnit2 <40171699+shahgithubnit2@users.noreply.github.com>
2019-03-04 16:41:38 +0530

    sopln statement added

--- src/com/nt/bank/Customer.java
+++ src/com/nt/bank/Customer.java
@@ -5,6 +5,7 @@ public class Customer {
 	public static void main(String[] args) {
 		SBIBank sbi=new SBIBank();
 			System.out.println(sbi.withDraw(999));
+			System.out.println("amount was succesfully debited");
 	}
 
 }
